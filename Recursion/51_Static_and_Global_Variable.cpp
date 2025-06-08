86b47acf9407237b64f3f11139c6e7fa145b6e11
Gautam-Rawat <rawatgammy2913@gmail.com>
2025-06-08 17:54:04 +0530

    Updating Section 51

--- Recursion/51_Static_and_Global_Variable.cpp
+++ Recursion/51_Static_and_Global_Variable.cpp
@@ -21,11 +21,38 @@ using namespace std;
 
 
 
+// int fun(int n){
+//     static int x=0;
+//     // printf("%d",x);
+//     if(n>0){
+//         x++;                    // the value of the static variable x will not change with every recursion
+//                                 // x -> 0, 1, 2, 3, 4, 5
+//         return fun(n-1)+x;
+//     }
+//     return 0;
+// }
+// int main(){
+//     int a=5;
+//     printf("%d\n",fun(a));
+//     printf("%d",fun(a));        // this time the function will not initialized as 0 but as 5 it was previous called
+
+//     return 0;
+// }
+// the result is different
+
+
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+// now we check for global variable
+
+int x=0;
+
 int fun(int n){
-    static int x=0;
+    // static int x=0;
     // printf("%d",x);
     if(n>0){
-        x++;                    // the value of the static variable x will not change with every recursion
+         x++;                    // the value of the static variable x will not change with every recursion
                                 // x -> 0, 1, 2, 3, 4, 5
         return fun(n-1)+x;
     }
@@ -34,8 +61,10 @@ int fun(int n){
 int main(){
     int a=5;
     printf("%d\n",fun(a));
-    printf("%d",fun(a));
+    printf("%d",fun(a));        // this time the function will not initialized as 0 but as 5 it was previous called
+
     return 0;
 }
-// the result is different
-
+//  we see that in both static and global variable example we saw the values are not reseting even if we recall the function
+//  static and global variable are not stored in stack memory 
+//  they are stored along with program
